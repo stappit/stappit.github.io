fa7189223f9f12b1b8eece627241d3ee81ccca46
Brian Callander <briancallander@gmail.com>
2018-07-02 22:06:08 +0200

    add berlin marathon post

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-stack build && stack exec site clean && git submodule add https://github.com/stappit/stappit.github.io _site  && stack exec site build
+stack build && stack exec site clean && git submodule add -f https://github.com/stappit/stappit.github.io _site && stack exec site build
